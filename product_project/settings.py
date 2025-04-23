ce4b100f9d9aaa759e5fc78ca445d4d20d8e8334
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2025-04-23 23:05:55 +0100

    Finishing up deployment config

--- product_project/settings.py
+++ product_project/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv  import load_dotenvfrom path
+from dotenv  import load_dotenv
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,8 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = DEBUG = os.getenv("DEBUG") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(';')
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(',')
 
 
 # Application definition
