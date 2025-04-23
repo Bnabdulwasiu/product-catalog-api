4b9c40740ad971e3ebf8431d85867424b802d6cb
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2025-04-23 23:00:37 +0100

    Environment variable config

--- product_project/settings.py
+++ product_project/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
-
+from dotenv  import load_dotenvfrom path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-213!d^v4e!x9!=a79r0ad_a-yfr%&1bm$28vl_wl1cd^wbm9rh'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = DEBUG = os.getenv("DEBUG") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(';')
 
 
 # Application definition
