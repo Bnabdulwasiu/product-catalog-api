29cd06905cb8d28db634ae79f4ce481d1ff59bd3
Abdulrahman Abdulwasiu <77780537+Bnabdulwasiu@users.noreply.github.com>
2025-04-24 15:09:45 +0100

    Updated schema config

--- product_project/urls.py
+++ product_project/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/products/', include('product.urls')),
-    #path("api/schema", SpectacularAPIView.as_view(), name='schema'),
+    path("api/schema", SpectacularAPIView.as_view(), name='schema'),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(
         url_name="schema"), name="swagger-ui"), 
 ]
