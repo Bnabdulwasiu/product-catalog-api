dd0b3566815e993796ff4beea4604ba33cc49d6d
Abdulrahman Abdulwasiu <77780537+Bnabdulwasiu@users.noreply.github.com>
2025-04-24 12:12:11 +0100

    Updated endpoint urls

--- product_project/urls.py
+++ product_project/urls.py
@@ -20,8 +20,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('product.urls')),
-    path("api/schema", SpectacularAPIView.as_view(), name='schema'),
+    path('api/products/', include('product.urls')),
+    #path("api/schema", SpectacularAPIView.as_view(), name='schema'),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(
         url_name="schema"), name="swagger-ui"), 
 ]
